9131c62449769c99b01eb8779201a8432e013466
liz <liz@inappstory.com>
2023-07-14 13:58:09 +0300

    clear up

--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
+//add dependencies
 const express = require("express");
 const app = express();
 const mustacheExpress = require("mustache-express");
 const dotenv = require("dotenv");
 
+//configure environment avriables
 const envConfig = dotenv.config();
 const PORT = envConfig.parsed.PORT;
 
-//set the views engine to mustache
+//set the view engine to mustache
 app.engine("mustache", mustacheExpress());
 app.set("view engine", "mustache");
 app.set("views", __dirname + "/views");
 
+//this function returns datal, that should be used in meta tags
 const viewData = (req, data) => {
   return {
     title: data.title,
@@ -20,16 +23,15 @@ const viewData = (req, data) => {
 };
 
 //routes
-//get root
+  //get root
 app.get("/", (req, res) => {
   res.render("main", {});
 });
 
-//get story by id
+  //get story by id
 app.get("/:storyid", async (req, res) => {
   try {
-    console.log(envConfig.parsed.API_KEY);
-    console.log(req.params.storyid);
+console.log(PORT);
     const apiResponse = await fetch(
       "https://api.inappstory.com/v2/story-share-page-info/" +
         req.params.storyid,
@@ -43,7 +45,7 @@ app.get("/:storyid", async (req, res) => {
     const apiResponseJSON = await apiResponse.json();
     console.log(apiResponseJSON);
 
-    res.render("template", viewData(req, apiResponseJSON));
+    res.render("sharePage", viewData(req, apiResponseJSON));
     
   } catch (err) {
     console.log(err);
